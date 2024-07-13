e1f7555aa904adcdedaf3d8b05d62e1509ecb0ff
Enrique Alberto Sifontes Romo <82631655+easr97@users.noreply.github.com>
2024-07-13 05:21:21 -0400

    5

--- public/assets/js/products.js
+++ public/assets/js/products.js
@@ -1,5 +1,24 @@
-async function getAllProducts(){
-    const res = await fetch("http://localhost:3003/produtcs");
-    const resJson = await res.json();
+fetch('http://yourWebSite.com/api/all')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        let table = document.querySelector('#tableId')
+        data.forEach(row => {
+            let tr = document.createElement('tr');
+            tr.appendChild(document.createElement('th').innerText(row.item1))
+            tr.appendChild(document.createElement('th').innerText(row.item2))
+            tr.appendChild(document.createElement('th').innerText(row.item3))
+            table.appendChild(tr)
+        })
+    });
 
+function mostrar() {
+    let archivo = document.getElementById("file").files[0];
+    let reader = new FileReader();
+    if (file) {
+        reader.readAsDataURL(archivo);
+        reader.onloadend = function () {
+            document.getElementById("img").src = reader.result;
+        }
+    }
 }
